8f4c8049a94747ab11ba941a5e4932eb9e8819f0
blake <blake@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-03-28 02:54:25 +0000

    Setting resolution to 256x224 Allowing the ability to toggle between fullscreen and windowed by pressing 'f'

--- source/main.py
+++ source/main.py
@@ -1,7 +1,11 @@
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+fullscreen = False
+
+output_screen = pygame.display.set_mode((800, 600))
+
+screen = pygame.Surface((256, 224))
 
 counter = 0
 
@@ -15,6 +19,12 @@ while scene != None:
 	for event in pygame.event.get():
 		if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
 			scene = None
+		elif event.type == KEYDOWN and event.key == K_f:
+			fullscreen = not fullscreen
+			if fullscreen:
+				output_screen = pygame.display.set_mode((800, 600), FULLSCREEN)
+			else:
+				output_screen = pygame.display.set_mode((800, 600))
 		else:
 			events.append(event)
 	
@@ -28,6 +38,8 @@ while scene != None:
 	screen.fill((0,0,0))
 	
 	scene.Render(screen)
+	
+	pygame.transform.scale(screen, (800, 600), output_screen)
 	
 	scene = scene.next
 	
