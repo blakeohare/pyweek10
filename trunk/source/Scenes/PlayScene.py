691db08bfafb012c2fdb695d2fe65875e9041994
blake <blake@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-03-28 08:34:00 +0000

    PlayScene stub.

---
+++ trunk/source/Scenes/PlayScene.py
@@ -0,0 +1,17 @@
+class PlayScreen:
+	def __init__(self, level):
+		self.counter = 0
+		self.next = self
+		self.level_id = level
+		wibbly_wobbly = False
+	
+	def ProcessInput(self, events):
+		pass
+	
+	def Update(self):
+		pass
+	
+	def Render(self, screen):
+		pass
+	
+	
