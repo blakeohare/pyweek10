35454cf9643af24713d0a30d754f9a18a8b98865
falun <falun@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-03-28 04:54:33 +0000

    CutSceneScene stub

---
+++ source/Scenes/CutSceneScene.py
@@ -0,0 +1,13 @@
+class CutSceneScene:
+   def __init__(self):
+      self.counter = 0
+
+   def ProcessInput(self, events):
+      pass
+   
+   def Render(self, screen):
+      # the screen will be 256x244 pixels
+      pass
+   
+   def Update(self):
+      self.counter += 1
