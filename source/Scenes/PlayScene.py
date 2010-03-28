8e06bdf14a3595f54a1b7a88ab6edba74c0643c5
blake <blake@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-03-28 23:55:21 +0000

    Initial incline check-in. There's still one more edge case (pun) I need to take care of before inclines will be done: - landing on an incline perpendicularly to its surface will allow you to fall through the ground

--- source/Scenes/PlayScene.py
+++ source/Scenes/PlayScene.py
@@ -1,3 +1,43 @@
+class Platform:
+	def __init__(self, type, left, y_left, width, y_right, height, jumpthrough):
+		self.type = type # jumpthrough, blocking, solid, incline
+		self.left = left
+		self.y_left = y_left
+		self.width = width
+		self.height = height # only used for solid type
+		self.y_right = y_right
+		self.jumpthrough = jumpthrough
+	
+	def get_y_at_x(self, x):
+		if self.type == 'incline':
+			percentage = (x - self.left + 0.0) / self.width
+			return int(self.y_right * percentage + self.y_left * (1 - percentage))
+		return self.y_left
+	
+	def is_x_in_range(self, x):
+		return self.left <= x and self.left + self.width >= x
+	
+	def get_left_wall_x(self):
+		return self.left
+	
+	def get_right_wall_x(self):
+		return self.left + self.width
+	
+	def get_top(self): #assumes non incline type
+		return self.y_left 
+	
+	def get_bottom(self):
+		if self.type == 'solid':
+			return self.y_left + self.height
+		return self.y_left #inclines aren't "bottomed"
+	
+	def get_closest_terminating_y(self, x):
+		if x < self.left:
+			return self.y_left
+		else:
+			return self.y_right
+		# this function will not be called when x is in the platform
+
 class PlayScreen:
 	def __init__(self, level):
 		self.counter = 0
@@ -9,23 +49,26 @@ class PlayScreen:
 		
 		wibbly_wobbly = False
 		
+		debug_offset = 0
 		self.platforms = {
 			'jumpthrough' : [ # left, top, width
-				[10, 150, 50],
-				[60, 150, 50],
-				[50, 102, 50]
+				Platform('jumpthrough', debug_offset + 0, 150, 40, 150, 0, True),
+				Platform('jumpthrough', debug_offset + 40, 150, 40, 150, 0, True),
+				Platform('jumpthrough', debug_offset + 120, 110, 40, 110, 0, True),
+				Platform('jumpthrough', debug_offset + 200, 150, 40, 150, 0, True)
 				],
 			
 			'blocking' : [ # left, top, width
-				[75, 54, 50]
+				Platform('blocking', debug_offset + 140, 40, 80, 40, 0, False)
 				],
 			
 			'solid' : [ #left, top, width, height
-				[50, 125, 50, 50]
+				Platform('solid', debug_offset + 220, 20, 30, 20, 50, False)
 				],
 			
-			'inclines' : [
-			
+			'inclines' : [ #left, left_top, width, right_top
+				Platform('incline', debug_offset + 80, 150, 40, 110, 0, True),
+				Platform('incline', debug_offset + 160, 110, 40, 150, 0, True)
 				]
 		}
 		
@@ -37,15 +80,15 @@ class PlayScreen:
 		return self.platforms['solid']
 	
 	def get_landing_surfaces_near(self, xy):
-		return self.platforms['jumpthrough'] + self.platforms['blocking'] + self.platforms['solid']
+		platforms = self.platforms['jumpthrough'] + self.platforms['blocking'] + self.platforms['solid']
+		x = xy[0]
+		for incline in self.platforms['inclines']:
+			if incline.is_x_in_range(x):
+				platforms.append(incline)
+		return platforms
 	
 	def get_ceilings(self, xy):
-		platforms = self.platforms['blocking'][:]
-		
-		for platform in self.platforms['solid']:
-			platforms.append([platform[0], platform[1] + platform[3], platform[2]])
-		
-		return platforms
+		return self.platforms['blocking'] + self.platforms['solid']
 	
 	def ProcessInput(self, events):
 		for event in events:
@@ -73,19 +116,19 @@ class PlayScreen:
 		
 		for sprite in self.sprites:
 			
-			
 			sprite.dx = sprite.vx
 			new_x = sprite.x + sprite.dx
-			#todo: make sure (new_x, sprite.y) is valid and CROSSABLE
 			
 			if sprite.dx > 0: #going right
 				wall = self.find_leftmost_wall_in_path(sprite.x, new_x, sprite.y)
 				if wall != None:
-					new_x = wall[0] - 1
+					new_x = wall.get_left_wall_x() - 1
 			elif sprite.dx < 0: #going left
 				wall = self.find_rightmost_wall_in_path(new_x, sprite.x, sprite.y)
 				if wall != None:
-					new_x = wall[0] + wall[2] + 1
+					new_x = wall.get_right_wall_x() + 1
+			
+			#TODO: fix jump through incline bug
 			
 			sprite.x = new_x
 			
@@ -122,31 +165,60 @@ class PlayScreen:
 					sprite.dy = 0
 					#TODO: play BONK noise
 					sprite.vy = 0
-					new_y = lowest[1] + y_offset
+					new_y = lowest.get_bottom() + y_offset
 					
 				else:
 					sprite.y = new_y
-				#TODO: blocking platforms
-				
 				
 			else:
 				if sprite.platform != None:
 					platform = sprite.platform
-					if sprite.x < platform[0] or sprite.x > platform[0] + platform[2]:
+					if platform.is_x_in_range(sprite.x):
+						self.set_sprite_on_platform(sprite, platform)
+					else:
 						# player walked off edge of platform
-						y = sprite.platform[1]
-						new_platform = self.find_highest_platform_in_path(sprite.x, y, y)
-						if new_platform != None:
-							self.set_sprite_on_platform(sprite, new_platform)
+						
+						new_platform_found = False
+						
+						# which way?
+						if sprite.x < platform.left:
+							# walked off left
+							for left_platform in self.get_landing_surfaces_near((platform.left - 1, sprite.y)):
+								
+								# check to see if the right side of this platform is vertically aligned with 
+								# the left side of the one you walked off
+								if abs(left_platform.left + left_platform.width - platform.left) <= 1:
+									
+									# check to see if they're vertically aligned
+									if abs(left_platform.y_right - platform.y_left) <= 1:
+										sprite.platform = left_platform
+										break
+								
 						else:
+							# walked off right
+							for right_platform in self.get_landing_surfaces_near((platform.left + platform.width + 1, sprite.y)):
+								
+								# check to see if the left side of this platform is vertically aligned with 
+								# the right side of the one you walked off
+								if abs(right_platform.left - (platform.left + platform.width)) <= 1:
+									
+									# check to see if they're vertically aligned
+									if abs(right_platform.y_left - platform.y_right) <= 1:
+										sprite.platform = right_platform
+										break
+						
+						# if no new platform was found...
+						if sprite.platform == platform:
+							#the sprite has fallen off the edge
 							sprite.on_ground = False
 							sprite.platform = None
+							
 				else:
 					# This shouldn't happen. But just in case...
 					sprite.on_ground = False
 	
 	def set_sprite_on_platform(self, sprite, platform):
-		sprite.y = int(platform[1] - sprite.height + sprite.height / 2) # odd math to keep consistent rounding
+		sprite.y = int(platform.get_y_at_x(sprite.x) - sprite.height + sprite.height / 2) # odd math to keep consistent rounding
 	
 	def find_leftmost_wall_in_path(self, left_x, right_x, y):
 		return self._find_first_wall_in_path(left_x, right_x, y, True)
@@ -161,15 +233,19 @@ class PlayScreen:
 		vicinity = ((left_x + right_x) / 2, y)
 		platforms = self.get_walls(vicinity)
 		
-		for platform in platforms:
-			if y >= platform[1] and y <= platform[1] + platform[3]:
-				wall_x = (platform[0] + platform[2], platform[0])[going_right]
+		for platform in platforms: # all platforms are guaranteed to be solid type
+			if y >= platform.get_top() and y <= platform.get_bottom():
+				if going_right:
+					wall_x = platform.left
+				else:
+					wall_x = platform.left + platform.width
+					
 				if left_x <= wall_x and right_x >= wall_x:
 					if furthest == None:
 						furthest = platform
-					elif going_right and furthest[0] > platform[0]:
+					elif going_right and furthest.left > platform.left:
 						furthest = platform
-					elif not going_right and furthest[0] + furthest[2] < platform[0] + platform[2]:
+					elif not going_right and furthest.left + furthest.width < platform.left + platform.width:
 						furthest = platform
 		return furthest
 		
@@ -183,13 +259,14 @@ class PlayScreen:
 			platforms = self.get_ceilings(vicinity)
 		
 		for platform in platforms: 
-			if x >= platform[0] and x <= platform[0] + platform[2]:
-				if upper_y <= platform[1] and lower_y >= platform[1]:
+			if platform.is_x_in_range(x):
+				platform_y = platform.get_y_at_x(x)
+				if upper_y <= platform_y and lower_y >= platform_y:
 					if furthest == None:
 						furthest = platform
-					elif going_down and furthest[1] > platform[1]:
+					elif going_down and furthest.get_y_at_x(x) > platform_y:
 						furthest = platform
-					elif not going_down and furthest[1] < platform[1]:
+					elif not going_down and furthest.get_bottom() < platform.get_bottom():
 						furthest = platform
 		
 		return furthest
@@ -205,23 +282,30 @@ class PlayScreen:
 		self.render_counter += 1
 		
 		for platform in self.platforms['jumpthrough']:
-			x = platform[0]
-			y = platform[1]
-			width = platform[2]
+			x = platform.left
+			y = platform.y_left
+			width = platform.width
 			pygame.draw.line(screen, (0, 0, 255), (x, y), (x + width, y))
 			
 		for platform in self.platforms['blocking']:
-			x = platform[0]
-			y = platform[1]
-			width = platform[2]
+			x = platform.left
+			y = platform.y_left
+			width = platform.width
 			pygame.draw.line(screen, (255, 0, 0), (x, y), (x + width, y))
 		
 		for platform in self.platforms['solid']:
-			x = platform[0]
-			y = platform[1]
-			width = platform[2]
-			height = platform[3]
+			x = platform.left
+			y = platform.y_left
+			width = platform.width
+			height = platform.height
 			pygame.draw.rect(screen, (100, 100, 100), Rect(x, y, width, height))
+		
+		for platform in self.platforms['inclines']:
+			x = platform.left
+			y_left = platform.y_left
+			width = platform.width
+			y_right = platform.y_right
+			pygame.draw.line(screen, (0, 0, 255), (x, y_left), (x + width, y_right))
 		
 		self.player.draw(screen, self.player.vx != 0, self.counter)
 		
