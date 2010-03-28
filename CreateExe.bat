18b51ad2e0917b4c2d048d1ff026d1385179a7f5
falun <falun@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-03-28 11:00:28 +0000

    quoted path to python

--- CreateExe.bat
+++ CreateExe.bat
@@ -1,5 +1,5 @@
 @echo OFF
-%py26%\python.exe util\concat.py
-%py26%\python.exe util\py2exe_setup.py
+"%py26%\python.exe" util\concat.py
+"%py26%\python.exe" util\py2exe_setup.py
 rmdir /q /s build
 move dist exe
