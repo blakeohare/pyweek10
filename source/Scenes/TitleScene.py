9881f1cdc644434f9cdcd13264544fc8f0bc2fd1
blake <blake@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-04-01 10:15:59 +0000

    Exit option from main menu

--- source/Scenes/TitleScene.py
+++ source/Scenes/TitleScene.py
@@ -11,21 +11,24 @@ class TitleScene:
 				if event.key in ('start', 'A', 'B'):
 					if self.index == 0:
 						self.next = SelectGameScene()
-					else:
+					elif self.index == 1:
 						self.next = ConfigureControlsScene()
+					else:
+						self.next = TransitionScene(self, None, 'fade_and_end', 30)
 				elif event.key == 'L':
 					self.next = CutSceneScene('demo')
 					
 				elif event.key == 'up':
 					self.index = max(0, self.index - 1)
 				elif event.key == 'down':
-					self.index = min(1, self.index + 1)
+					self.index = min(2, self.index + 1)
 	
 	def Render(self, screen):
 		screen.blit(images.Get('title.png'), (0, 0))
-		screen.blit(get_text('START'), (60, 160))
-		screen.blit(get_text('CONFIGURE CONTROLS'), (60, 180))
-		screen.blit(images.Get('title_cursor.png'), (48, 160 + self.index * 20))
+		screen.blit(get_text('Start'), (60, 150))
+		screen.blit(get_text('Configure Controls'), (60, 170))
+		screen.blit(get_text('Exit'), (60, 190))
+		screen.blit(images.Get('title_cursor.png'), (48, 152 + self.index * 20))
 	
 	def Update(self):
 		self.counter += 1
