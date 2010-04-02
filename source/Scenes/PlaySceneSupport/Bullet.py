00d38bcc5bf46a2258b07e4e9c9878ba4e4b8d2e
blake <blake@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-04-02 07:49:38 +0000

    Chargeable wand

--- source/Scenes/PlaySceneSupport/Bullet.py
+++ source/Scenes/PlaySceneSupport/Bullet.py
@@ -1,5 +1,5 @@
 class Bullet:
-	def __init__(self, going_left, x, y, magic):
+	def __init__(self, going_left, x, y, magic, charge_level=0):
 		self.x = x
 		self.y = y
 		self.going_left = going_left
@@ -8,9 +8,14 @@ class Bullet:
 		self.name = ['basic','ice','fire','lightning','charge'][magic]
 		self.expired = False
 		self.lifetime = 0
+		self.charge = charge_level
+		if self.magic == 4:
+			self.charge_text = '_'+('small','medium','large')[self.charge]
+		else:
+			self.charge_text = ''
 		
 	def draw(self, screen, camera_x, camera_y):
-		file = 'wands/'+(('', 'left/')[self.going_left])+'shoot_' + self.name + str(int((self.lifetime / 2) % 3)) + '.png'
+		file = 'wands/'+(('', 'left/')[self.going_left])+'shoot_' + self.name + self.charge_text + str(int((self.lifetime / 2) % 3)) + '.png'
 		img = images.Get(file)
 		x = self.x - camera_x - int(img.get_width() / 2)
 		y = self.y - camera_y - int(img.get_height() / 2)
