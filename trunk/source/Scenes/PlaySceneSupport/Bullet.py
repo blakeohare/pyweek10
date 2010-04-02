b7116ed0e6404a0794472b89d066e977cccc2b4d
blake <blake@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-04-02 01:07:29 +0000

    Throwin' bricks

---
+++ trunk/source/Scenes/PlaySceneSupport/Bullet.py
@@ -0,0 +1,32 @@
+class Bullet:
+	def __init__(self, going_left, x, y, magic):
+		self.x = x
+		self.y = y
+		self.going_left = going_left
+		self.x += (8,-8)[going_left]
+		self.magic = magic # selected wand index at the moment of firing
+		self.name = ['basic','ice','fire','lightning','charge'][magic]
+		self.expired = False
+		self.lifetime = 0
+		
+	def draw(self, screen, camera_x, camera_y):
+		file = 'wands/shoot_' + self.name + str(int((self.lifetime / 2) % 3)) + '.png'
+		img = images.Get(file)
+		x = self.x - camera_x - int(img.get_width() / 2)
+		y = self.y - camera_y - int(img.get_height() / 2)
+		screen.blit(img, (x, y))
+		
+	def update(self):
+		self.lifetime += 1
+		if self.going_left:
+			self.x -= 6
+		else:
+			self.x += 6
+	
+	def is_off_screen(self, left, right):
+		return self.x < left - 20 or self.x > right + 20
+	
+	def void_this(self):
+		self.expired = True
+		
+	
