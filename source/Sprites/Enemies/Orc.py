073f97a9d98c8c5919098af000d1c6da2b3c705b
blake <blake@3f0623ec-4a82-0410-8343-83e9d6098c66>
2010-04-03 09:46:59 +0000

    Press 8 for Orc.

---
+++ source/Sprites/Enemies/Orc.py
@@ -0,0 +1,32 @@
+
+class EnemyOrc(Sprite):
+	
+	def __init__(self, x, y):
+		self.lifetime = 0
+		Sprite.__init__(self, x, y)
+		self.width = 15
+		self.height = 34
+		self.left_facing = True
+		
+	def draw(self, surface, is_moving, counter, camera_offset):
+		
+		direction = ('right', 'left')[self.left_facing]
+		
+		file = direction + str(int(int(counter / 4) % 3)) + '.png'
+		
+		img = images.Get('sprites/Enemies/Orclike/' + file)
+		
+		xy = self.get_top_left()
+		x = xy[0] - camera_offset[0]
+		y = xy[1] - camera_offset[1]
+		
+		surface.blit(img, (x, y))
+	
+	def update(self, playScene):
+		self.lifetime += 1
+		
+		self.vx = (1, -1)[self.left_facing]
+		if self.is_going_to_vx_bad(playScene):
+			self.vx *= -1
+			self.left_facing = not self.left_facing
+	
